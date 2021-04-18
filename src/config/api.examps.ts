238402af2284a7130a94d1d075ab615019919f04
97Fakhreddine <fakhri19971998@gmail.com>
2021-04-18 21:56:02 +0100

    form has been created with success

---
+++ src/config/api.examps.ts
@@ -0,0 +1,12 @@
+/**
+ * @GOOGLE_SHEET_API_LINK {string}
+ * Open a new Spreadsheet by clicking File, then New, and then Spreadsheet.
+ * Name the sheet to name of your choice and save.
+ * Click the share button on the top right of your screen, and edit the permission to public.
+ * copy the link and go to https://sheet.best/ and create your free account.
+ * Create a new connection and paste your copied URL from the Google Sheets in the connection URL box.
+ * Click on connect. You'll be redirected to your connections page. Here, you can see all your connections. Click on the details of your new connection.
+ * Copy the CONNECTION URL. This URL will be used as the endpoint for sending POST Requests.
+ */
+
+export const GOOGLE_SHEET_API_LINK = "Fill this place with your api link";
