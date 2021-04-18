53928394cfea32b3acbb45d366e83a2fb7f84b35
97Fakhreddine <fakhri19971998@gmail.com>
2021-04-18 16:43:40 +0100

    init

---
+++ src/pages/Form.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+import { Button, Form, Container, Header } from "semantic-ui-react";
+import "../style/Form.css";
+
+export const FormComponent: React.FC<{}> = (props) => {
+  return <div>hello</div>;
+};
